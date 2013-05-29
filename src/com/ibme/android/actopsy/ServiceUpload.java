a91a900b8bdc0509d8ddf9abc0fc9660cc587b48
Maxim Osipov <maxim.osipov@gmail.com>
2013-05-29 11:08:54 +0100

    Change to BSD 3-Clause license
    Signed-off-by: Maxim Osipov <maxim.osipov@gmail.com>

--- src/com/ibme/android/actopsy/ServiceUpload.java
+++ src/com/ibme/android/actopsy/ServiceUpload.java
@@ -1,18 +1,30 @@
 //
-// Copyright (C) 2013 Maxim Osipov
+// Copyright (C) 2013, Maxim Osipov
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// All rights reserved.
 //
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//  - Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//  - Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//  - Neither the name of the University of Oxford nor the names of its
+//    contributors may be used to endorse or promote products derived from this
+//    software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
 package com.ibme.android.actopsy;
@@ -93,8 +105,8 @@ public class ServiceUpload extends Service implements OnSharedPreferenceChangeLi
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		int localStorage = Integer.valueOf(prefs.getString("editLocalStorage", "10")); 
 		mDeleteAge = localStorage*ClassConsts.MILLIDAY;
-		mUserID = prefs.getString("editUserID", "anonymous"); 
-		mUserPass = prefs.getString("editUserPass", "password"); 
+		mUserID = prefs.getString("editUserID", "anonymous");
+		mUserPass = prefs.getString("editUserPass", "password");
 
 		// Prepare for connectivity tracking
 		mConnectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -244,8 +256,8 @@ public class ServiceUpload extends Service implements OnSharedPreferenceChangeLi
 					// Prepare HTTP request
 					MultipartEntity entity = new MultipartEntity();
 					entity.addPart( "MAX_FILE_SIZE", new StringBody(ClassConsts.UPLOAD_SIZE));
-					entity.addPart( "USER", new StringBody(mUserID));        	 
-					entity.addPart( "PASS", new StringBody(mUserPass));        	 
+					entity.addPart( "USER", new StringBody(mUserID));
+					entity.addPart( "PASS", new StringBody(mUserPass));
 					entity.addPart( "FILE", new FileBody(files[i]));        	 
 					httppost.setEntity( entity );
 
@@ -315,10 +327,10 @@ public class ServiceUpload extends Service implements OnSharedPreferenceChangeLi
 			}
 		} else if (key.equals("editUserID")) {
 			Log.i(TAG, "Changed user ID");
-			mUserID = sharedPreferences.getString("editUserID", "anonymous"); 
+			mUserID = sharedPreferences.getString("editUserID", "anonymous");
 		} else if (key.equals("editUserPass")) {
 			Log.i(TAG, "Changed password");
-			mUserPass = sharedPreferences.getString("editUserPass", "password"); 
+			mUserPass = sharedPreferences.getString("editUserPass", "password");
 		}
 	}
 }
